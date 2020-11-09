22ab002283a3cbc34e0d68f6b1bcba6d506029a3
Igor Shaposhnikov <jourloy@yandex.ru>
2020-11-09 16:09:41 +0300

    Remove log

--- src/tools.js
+++ src/tools.js
@@ -3,9 +3,7 @@ const tools = {
         if (data.indexOf(':tmi.twitch.tv') != -1) return null;
         const splitedData = data.split(':');
         if (splitedData.length < 3 || splitedData.length > 3) return null;
-        console.log(splitedData);
         const username = splitedData[1].split('!');
-        console.log(username)
         return username[0];
     },
 
